53d890bb3d14c55f2e84b63be2451b2e530f66b1
adars <adars.nepal@gmail.com>
2023-03-06 18:10:30 +0000

    fixed table commit

--- src/Component/summaryReport.js
+++ src/Component/summaryReport.js
@@ -4,18 +4,6 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 
-// {[
-//   "Client Name",  input? input.clientName :"Johny",
-//   "Current Age:", input?input.currentAge:"",           
-//   "RetirementAge:",input?input.retirementAge : "",
-//   "Current Savings: $" ,input?input.currentSavings : "",
-//   "Annual Household Income: $", input?input.annualHouseholdIncome : "",
-//   "Expected Income Increased:",input?input.expectedII : "",
-//   "Estimated Monthly Retirement Expenses: ",input?input.estimatedRE : "",
-//   "Expected Rate of Return:" , input?input.expectedROR : "" ,
-//   "Retirement Savings Rate: " ,input?input.retirementSR : ""           
-// ]}
-
 function SummaryReport({data}){
     let input = data.form_input_values;
     const [checked, setChecked] = React.useState(false);
@@ -50,8 +38,6 @@ function SummaryReport({data}){
           </Table>
         </TableContainer>
       </div>
-
-        // /* <p>You need to save an additional ${additionalSavingsNeeded.toFixed(2)} to meet your retirement goal of ${totalSavingsNeeded.toFixed(2)}.</p>  */}
     );
 }
 export default SummaryReport;
